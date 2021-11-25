8cdbd1dccb3b20a3453e0124b8dcdb597bc1fb5f
Yong Hoon <oyhun00@gmail.com>
2021-11-26 00:09:57 +0900

    feat: 포스트 디테일 레이아웃 구성

--- components/Post/Post.jsx
+++ components/Post/Post.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import { Typography, Divider, Breadcrumb } from 'antd';
+import styled from '@emotion/styled';
 
+const { Title, Paragraph, Text } = Typography;
 const Post = () => {
   return (
-    <div>Post</div>
+    <Typography>
+      <CustomTitle>첫번째 포스트 제목</CustomTitle>
+      <Breadcrumb>
+        <Breadcrumb.Item>21. 03. 22</Breadcrumb.Item>
+        <Breadcrumb.Item>portfolio</Breadcrumb.Item>
+      </Breadcrumb>
+      <Divider />
+      <Paragraph>
+        simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently wit
+      </Paragraph>
+    </Typography>
   )
 };
+
+const CustomTitle = styled(Title)`
+  color: #fff !important;
+`;
 
 export default Post;
--- components/Post/PostBox.jsx
+++ components/Post/PostBox.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
+import Link from 'next/link';
 import { Card } from 'antd';
 import styled from '@emotion/styled';
 
 const { Meta } = Card;
 const PostBox = () => {
   return (
-    <CustomCard
-      hoverable
-      cover={<img alt="example" src="https://static.vecteezy.com/packs/media/components/global/search-explore-nav/img/vectors/term-bg-1-666de2d941529c25aa511dc18d727160.jpg" />}
-    >
-      <Meta title="포스트 박스 테스트!!!!!!" description="s simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It "></Meta>
-    </CustomCard>
+    <Link href="/post/1">
+      <CustomCard
+        hoverable
+        cover={<img alt="example" src="https://static.vecteezy.com/packs/media/components/global/search-explore-nav/img/vectors/term-bg-1-666de2d941529c25aa511dc18d727160.jpg" />}
+      >
+        <Meta title="포스트 박스 테스트!!!!!!" description="s simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It "></Meta>
+      </CustomCard>
+    </Link>
   )
 };
 
