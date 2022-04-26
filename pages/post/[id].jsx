4e745252bffa07293af92ccb03a7ac2c0bbea30b
Yong Hoon <oyhun00@gmail.com>
2022-04-26 16:41:59 +0900

    fix: getServerSideProps 로 수정

--- pages/post/[id].jsx
+++ pages/post/[id].jsx
@@ -1,6 +1,5 @@
 import { END } from 'redux-saga';
 import dynamic from 'next/dynamic';
-import axios from 'axios';
 
 import wrapper from '@Store/configure';
 import { GET_POST_REQUEST } from '@Constants/actionTypes';
@@ -9,16 +8,7 @@ const Post = dynamic(() => import('@Components/Post/Post'));
 
 const PostDetail = () => <Post />;
 
-export async function getStaticPaths() {
-  const { data } = await axios.get(`${process.env.APP_BASE_URL}/api/post/ids`);
-  const paths = data.map((v) => ({
-    params: { id: v.id.toString() },
-  }));
-
-  return { paths, fallback: false };
-}
-
-export const getStaticProps = wrapper.getStaticProps((store) => async ({ params }) => {
+export const getServerSideProps = wrapper.getServerSideProps((store) => async ({ params }) => {
   const { id } = params;
 
   store.dispatch({ type: GET_POST_REQUEST, payload: parseInt(id, 10) });
